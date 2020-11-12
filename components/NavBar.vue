cd1b1832f70227d9d557c51022d040453ddd16cc
Julien Arrault <julien.arrault.pro@gmail.com>
2020-11-12 20:05:48 +0100

    Adding Navbar, Footer and Home page layouts

---
+++ components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <nuxt-link class="navbar-brand" to="#/">
+        conduit
+      </nuxt-link>
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <nuxt-link class="nav-link" to="#/">
+            Home
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" to="">
+            <i class="ion-compose" />&nbsp;New Post
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" to="">
+            <i class="ion-gear-a" />&nbsp;Settings
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" to="">
+            Sign up
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
