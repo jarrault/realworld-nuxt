fd47f1b9e80b9f8c4eed8185ea27a64aba83f308
Julien Arrault <julien.arrault@gmail.com>
2021-01-20 19:38:33 +0100

    Adding follow user feature

--- components/ArticleMeta.vue
+++ components/ArticleMeta.vue
@@ -8,10 +8,9 @@
     <nuxt-link v-if="isAuthorOfTheArticle" class="btn btn-outline-secondary btn-sm" :to="{ name: 'editor-slug', params: { slug: article.slug }}">
       <i class="ion-edit" /> Edit Article
     </nuxt-link>
-    <button v-else class="btn btn-sm btn-outline-secondary">
+    <button v-else class="btn btn-sm btn-outline-secondary" @click="toggleFollow()">
       <i class="ion-plus-round" />
-      &nbsp; Follow {{ article.author.username }}
-      <span v-if="article.author.following" class="counter">({{ article.author.following }})</span>
+      <span v-if="!article.author.following">Follow</span> <span v-else @click="unfollowAuthor()">Unfollow</span> {{ article.author.username }}
     </button>
     &nbsp;&nbsp;
     <button v-if="isAuthorOfTheArticle" class="btn btn-outline-danger btn-sm" @click="deleteArticle()">
@@ -49,6 +48,15 @@ export default {
     async deleteArticle () {
       await this.$axios.delete(`/articles/${this.article.slug}`)
       this.$router.push('/')
+    },
+    async toggleFollow () {
+      if (!this.article.author.following) {
+        await this.$axios.post(`/profiles/${this.article.author.username}/follow`)
+      } else {
+        await this.$axios.delete(`/profiles/${this.article.author.username}/follow`)
+      }
+
+      this.article.author.following = !this.article.author.following
     }
   }
 }
